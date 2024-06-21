68d7a0ec6549ac78744f2383ef2568992a8dc300
ZaK3939 <tsu.aria.kalei@gmail.com>
2024-06-21 23:32:54 +0400

    feat: update dojoconfig

--- client/src/dojoConfig.ts
+++ client/src/dojoConfig.ts
@@ -1,6 +1,9 @@
 import devManifest from "../../manifests/dev/manifest.json";
 import slotManifest from "../../manifests/slot/manifest.json";
-import { createDojoConfig } from "@dojoengine/core";
+import {
+    KATANA_ETH_CONTRACT_ADDRESS,
+    createDojoConfig,
+} from "@dojoengine/core";
 
 const manifest =
     import.meta.env.VITE_PUBLIC_MANIFEST_PROFILE === "slot"
@@ -9,4 +12,11 @@ const manifest =
 
 export const dojoConfig = createDojoConfig({
     manifest,
+    rpcUrl: import.meta.env.VITE_PUBLIC_NODE_URL,
+    toriiUrl: import.meta.env.VITE_PUBLIC_TORII,
+    masterAddress: import.meta.env.VITE_PUBLIC_MASTER_ADDRESS,
+    masterPrivateKey: import.meta.env.VITE_PUBLIC_MASTER_PRIVATE_KEY,
+    accountClassHash: import.meta.env.VITE_PUBLIC_ACCOUNT_CLASS_HASH,
+    feeTokenAddress: KATANA_ETH_CONTRACT_ADDRESS,
 });
+
